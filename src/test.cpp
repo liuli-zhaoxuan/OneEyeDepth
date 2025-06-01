aa632ca732dbe196fc8cfa2ae1a8515edcb89cc6
liuli-zhaoxuan <liulizhaoxuan@gmail.com>
2025-06-01 17:48:54 +0800

    完成了图像路径选择、基准图像显示功能； 预留了图像畸变矫正接口

--- src/test.cpp
+++ src/test.cpp
@@ -1,13 +1,61 @@
 #include "test.h"
+#include <QFileDialog>
+#include <QGraphicsPixmapItem>
+#include <QMessageBox>
+#include <QDir>
 
 test::test(QWidget* parent)
     : QMainWindow(parent)
     , ui(new Ui::test)
 {
     ui->setupUi(this);
+    scene = new QGraphicsScene(this);
+    ui->graphicsView->setScene(scene);
 }
 
 test::~test()
 {
     delete ui;
+}
+
+void test::on_selectPathButton_clicked()
+{
+    selectedPath = QFileDialog::getExistingDirectory(this, tr("选择图片目录"), "");
+    if (selectedPath.isEmpty()) {
+        QMessageBox::warning(this, "提示", "未选择任何目录");
+    } else {
+        ui->label_path->setText(selectedPath);
+        ui->label_filename->setText("文件: （未选择）");
+    }
+}
+
+void test::on_openImageButton_clicked()
+{
+    if (selectedPath.isEmpty()) {
+        QMessageBox::warning(this, "错误", "请先选择图片路径");
+        return;
+    }
+
+    QString fileName = QFileDialog::getOpenFileName(this, tr("打开图像"), selectedPath, tr("Image Files (*.png *.jpg *.bmp *.tif *.jpeg)"));
+    if (!fileName.isEmpty()) {
+        QPixmap pixmap(fileName);
+        if (!pixmap.isNull()) {
+            scene->clear();
+            scene->addPixmap(pixmap);
+            ui->graphicsView->fitInView(scene->itemsBoundingRect(), Qt::KeepAspectRatio);
+            ui->label_filename->setText(QFileInfo(fileName).fileName());
+        } else {
+            QMessageBox::critical(this, "错误", "加载图像失败");
+        }
+    }
+}
+
+void test::on_loadIntrinsicsButton_clicked()
+{
+    //opencv需要重新编译
+}
+
+void test::on_undistortButton_clicked()
+{
+    
 }
--- src/test.h
+++ src/test.h
@@ -1,14 +1,25 @@
 #pragma once
 #include <QMainWindow>
 #include "ui_test.h"
+#include <QGraphicsScene>
+#include <QString>
 
 class test : public QMainWindow {
     Q_OBJECT
 
 public:
-    test(QWidget* parent = nullptr);
+    explicit test(QWidget* parent = nullptr);
     ~test();
+
+private slots:
+    void on_selectPathButton_clicked();
+    void on_openImageButton_clicked();
+    void on_loadIntrinsicsButton_clicked();
+    void on_undistortButton_clicked();
 
 private:
     Ui::test *ui;
+    QGraphicsScene *scene;
+    QString selectedPath;
+    //cv::Mat cameraMatrix, distCoeffs;
 };
